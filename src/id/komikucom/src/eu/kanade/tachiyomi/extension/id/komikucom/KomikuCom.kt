9bb46550d35d4707b02437af5314e75bdc2c66cf
imgview <komiklah007@gmail.com>
2024-10-12 11:30:12 +0700

    Update KomikuCom.kt

--- src/id/komikucom/src/eu/kanade/tachiyomi/extension/id/komikucom/KomikuCom.kt
+++ src/id/komikucom/src/eu/kanade/tachiyomi/extension/id/komikucom/KomikuCom.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.revived.id.komikucom
+package eu.kanade.tachiyomi.extension.id.komiktap
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.source.model.Page
