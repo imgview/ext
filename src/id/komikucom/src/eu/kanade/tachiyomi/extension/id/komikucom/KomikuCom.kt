37f78d0c46b1d78c50fc55956e0da54f8bba6fd3
imgview <komiklah007@gmail.com>
2024-10-12 11:06:44 +0700

    Update KomikuCom.kt

--- src/id/komikucom/src/eu/kanade/tachiyomi/extension/id/komikucom/KomikuCom.kt
+++ src/id/komikucom/src/eu/kanade/tachiyomi/extension/id/komikucom/KomikuCom.kt
@@ -1,7 +1,20 @@
-package eu.kanade.tachiyomi.extension.id.komikucom
+package eu.kanade.tachiyomi.revived.id.komikucom
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.PageList
+import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class KomikuCom : MangaThemesia("Komiku.com", "https://komiku.com", "id", dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("id")))
+class KomikuCom : MangaThemesia("Komiku.com", "https://komiku.com", "id", dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("id"))) {
+
+    override fun pageListParse(document: Document): List<Page> {
+        return document.select("img[src]").mapIndexed { i, element ->
+            val imageUrl = element.attr("abs:src")
+            val resizedImageUrl = "https://resize.sardo.work/?width=300&quality=75&imageUrl=$imageUrl"
+            Page(i, "", resizedImageUrl)
+        }
+    }
+}
