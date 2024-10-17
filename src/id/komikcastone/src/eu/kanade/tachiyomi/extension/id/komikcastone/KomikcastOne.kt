0d4472dba241c2cd4a36d7c8f9bad422a18f276b
imgview <komiklah007@gmail.com>
2024-10-17 19:07:16 +0700

    Update KomikcastOne.kt

--- src/id/komikcastone/src/eu/kanade/tachiyomi/extension/id/komikcastone/KomikcastOne.kt
+++ src/id/komikcastone/src/eu/kanade/tachiyomi/extension/id/komikcastone/KomikcastOne.kt
@@ -2,8 +2,6 @@ package eu.kanade.tachiyomi.extension.id.komikcastone
 
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
-import eu.kanade.tachiyomi.source.model.Filter
-import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
