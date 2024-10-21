8c6b3d45cbd9492ee801e4d66e0ac98daed14d3a
imgview <komiklah007@gmail.com>
2024-10-21 13:17:06 +0700

    Update KC.kt

--- src/id/kc/src/eu/kanade/tachiyomi/extension/id/kc/KC.kt
+++ src/id/kc/src/eu/kanade/tachiyomi/extension/id/kc/KC.kt
@@ -18,22 +18,22 @@ import java.util.Locale
 
 class KC : ParsedHttpSource() {
     override val name = "KC"
-    override val baseUrl = "https://komikindo.lol"
+    override val baseUrl = "https://komikcast.one"
     override val lang = "id"
     override val supportsLatest = true
     override val client: OkHttpClient = network.cloudflareClient
     private val dateFormat: SimpleDateFormat = SimpleDateFormat("MMM d, yyyy", Locale.US)
 
-    // similar/modified theme of "https://bacakomik.co"
+    // Mod dari Komikindo.lol"
     override fun popularMangaRequest(page: Int): Request {
-        return GET("$baseUrl/daftar-manga/page/$page/?order=popular", headers)
+        return GET("$baseUrl/komik-berwarna/page/$page/?order=popular", headers)
     }
 
     override fun latestUpdatesRequest(page: Int): Request {
-        return GET("$baseUrl/daftar-manga/page/$page/?order=update", headers)
+        return GET("$baseUrl/komik-berwarna/page/$page/?order=update", headers)
     }
 
-    override fun popularMangaSelector() = "div.animepost"
+    override fun popularMangaSelector() = "div.post-item"
     override fun latestUpdatesSelector() = popularMangaSelector()
     override fun searchMangaSelector() = popularMangaSelector()
 
@@ -46,37 +46,37 @@ class KC : ParsedHttpSource() {
 
     override fun searchMangaFromElement(element: Element): SManga {
         val manga = SManga.create()
-        manga.thumbnail_url = element.select("div.limit img").attr("src")
-        manga.title = element.select("div.tt h4").text()
-        element.select("div.animposx > a").first()!!.let {
+        manga.thumbnail_url = element.select("div.post-item-thumb img").attr("src")
+        manga.title = element.select("div.post-item-title h4").text()
+        element.select("div.post-item-box > a").first()!!.let {
             manga.setUrlWithoutDomain(it.attr("href"))
         }
         return manga
     }
 
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
-        val url = "$baseUrl/daftar-manga/page/$page/".toHttpUrl().newBuilder()
+        val url = "$baseUrl/daftar-komik/page/$page/".toHttpUrl().newBuilder()
             .addQueryParameter("title", query)
         return GET(url.build(), headers)
     }
 
     override fun mangaDetailsParse(document: Document): SManga {
-        val infoElement = document.select("div.infoanime").first()!!
+        val infoElement = document.select("div.info-chapter-manga").first()!!
         val descElement = document.select("div.desc > .entry-content.entry-content-single").first()!!
         val manga = SManga.create()
-        val authorCleaner = document.select(".infox .spe b:contains(Pengarang)").text()
-        manga.author = document.select(".infox .spe span:contains(Pengarang)").text().substringAfter(authorCleaner)
-        val artistCleaner = document.select(".infox .spe b:contains(Ilustrator)").text()
-        manga.artist = document.select(".infox .spe span:contains(Ilustrator)").text().substringAfter(artistCleaner)
+        val authorCleaner = document.select(".info-chapter-manga-box .col-info-manga-box b:contains(Author)").text()
+        manga.author = document.select(".info-chapter-manga-box .col-info-manga-box b:contains(Author)").text().substringAfter(authorCleaner)
+        val artistCleaner = document.select(".info-chapter-manga-box .col-info-manga-box b:contains(Artis)").text()
+        manga.artist = document.select(".info-chapter-manga-box .col-info-manga-box b:contains(Artis)").text().substringAfter(artistCleaner)
         val genres = mutableListOf<String>()
-        infoElement.select(".infox .genre-info a, .infox .spe span:contains(Grafis:) a, .infox .spe span:contains(Tema:) a, .infox .spe span:contains(Konten:) a, .infox .spe span:contains(Jenis Komik:) a").forEach { element ->
+        infoElement.select(".info-chapter-manga-box genre-info-manga a").forEach { element ->
             val genre = element.text()
             genres.add(genre)
         }
         manga.genre = genres.joinToString(", ")
-        manga.status = parseStatus(infoElement.select(".infox > .spe > span:nth-child(2)").text())
+        manga.status = parseStatus(infoElement.select(".info-chapter-manga-box > .col-info-manga-box > span:nth-child(2)").text())
         manga.description = descElement.select("p").text().substringAfter("bercerita tentang ")
-        val altName = document.selectFirst(".infox > .spe > span:nth-child(1)")?.text().takeIf { it.isNullOrBlank().not() }
+        val altName = document.selectFirst(".info-chapter-manga-box > .col-info-manga-box > span:nth-child(1)")?.text().takeIf { it.isNullOrBlank().not() }
         altName?.let {
             manga.description = manga.description + "\n\n$altName"
         }
@@ -93,11 +93,11 @@ class KC : ParsedHttpSource() {
     override fun chapterListSelector() = "#chapter_list li"
 
     override fun chapterFromElement(element: Element): SChapter {
-        val urlElement = element.select(".lchx a").first()!!
+        val urlElement = element.select(".list-chapter-chapter a").first()!!
         val chapter = SChapter.create()
         chapter.setUrlWithoutDomain(urlElement.attr("href"))
         chapter.name = urlElement.text()
-        chapter.date_upload = element.select(".dt a").first()?.text()?.let { parseChapterDate(it) } ?: 0
+        chapter.date_upload = element.select(".list-chapter-date a").first()?.text()?.let { parseChapterDate(it) } ?: 0
         return chapter
     }
 
@@ -153,11 +153,11 @@ class KC : ParsedHttpSource() {
     override fun pageListParse(document: Document): List<Page> {
         val pages = mutableListOf<Page>()
         var i = 0
-        document.select("div.img-landmine img").forEach { element ->
+        document.select("div.oi_ada_class_skrng img").forEach { element ->
             val url = element.attr("onError").substringAfter("src='").substringBefore("';")
             i++
             if (url.isNotEmpty()) {
-                val resizedImageUrl = "https://resize.sardo.work/?width=300&quality=75&imageUrl=$url"
+                val resizedImageUrl = "https://images.weserv.nl/?w=300&q=70&url=https://x.0ms.dev/q70/$url"
                 pages.add(Page(i, "", resizedImageUrl))
             }
         }
@@ -168,7 +168,7 @@ class KC : ParsedHttpSource() {
 
     override fun getFilterList(): FilterList {
     return FilterList(
-        Filter.Header("NOTE: Ignored if using text search!")
+        Filter.Header("Mod by: SupKelelawar")
     )
-}
+  }
 }
