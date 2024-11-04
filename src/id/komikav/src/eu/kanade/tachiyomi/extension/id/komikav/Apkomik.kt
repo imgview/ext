2290cd5ee412bdd38e0a1dbb7a3aef4fc70e7c5d
imgview <komiklah007@gmail.com>
2024-11-04 11:28:47 +0700

    Update Apkomik.kt

--- src/id/komikav/src/eu/kanade/tachiyomi/extension/id/komikav/Apkomik.kt
+++ src/id/komikav/src/eu/kanade/tachiyomi/extension/id/komikav/Apkomik.kt
@@ -1,7 +1,22 @@
 package eu.kanade.tachiyomi.extension.id.komikav
 
+import android.app.Application
+import androidx.preference.EditTextPreference
+import androidx.preference.PreferenceScreen
+import eu.kanade.tachiyomi.lib.domain.Domain
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import eu.kanade.tachiyomi.source.ConfigurableSource
+import eu.kanade.tachiyomi.source.model.Filter
+import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.Page
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import okhttp3.Request
+import org.jsoup.nodes.Document
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -9,14 +24,144 @@ class Apkomik : MangaThemesia(
     "APKOMIK",
     "https://apkomik.cc",
     "id",
-    dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale.forLanguageTag("id")),
-) {
-    // Formerly "Komik AV (WP Manga Stream)"
-    override val id = 7875815514004535629
+    "/manga",
+    dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("id"))
+), ConfigurableSource {
+
+    private val preferences = Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)
+
+    private fun getPrefCustomUA(): String {
+        return preferences.getString("custom_ua", "Default User-Agent") ?: "Default User-Agent"
+    }
+
+    private fun getResizeServiceUrl(): String {
+        return preferences.getString("resize_service_url", "https://resize.sardo.work/?width=300&quality=75&imageUrl=") ?: "https://resize.sardo.work/?width=300&quality=75&imageUrl="
+    }
+
+    override var baseUrl = preferences.getString(BASE_URL_PREF, "https://komiku.com")!!
 
     override val client = super.client.newBuilder()
-        .rateLimit(4)
+        .addInterceptor { chain ->
+            val original = chain.request()
+            val requestBuilder = original.newBuilder()
+                .header("User-Agent", getPrefCustomUA())
+            chain.proceed(requestBuilder.build())
+        }
+        .rateLimit(1)
         .build()
 
-    override val hasProjectPage = true
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        return if (query.isEmpty()) {
+            super.searchMangaRequest(page, query, filters)
+        } else {
+            GET("$baseUrl/?s=$query&page=$page", headers)
+        }
+    }
+
+    override fun mangaDetailsParse(document: Document) = super.mangaDetailsParse(document).apply {
+        title = document.selectFirst(seriesThumbnailSelector)!!.attr("alt")
+    }
+
+    override fun getFilterList(): FilterList {
+        val filters = mutableListOf<Filter<*>>(
+            Filter.Header("Note: Can't be used with text search!"),
+            Filter.Separator(),
+            StatusFilter(intl["status_filter_title"], statusOptions),
+            TypeFilter(intl["type_filter_title"], typeFilterOptions),
+            OrderByFilter(intl["order_by_filter_title"], orderByFilterOptions),
+        )
+        if (!genrelist.isNullOrEmpty()) {
+            filters.addAll(
+                listOf(
+                    Filter.Header(intl["genre_exclusion_warning"]),
+                    GenreListFilter(intl["genre_filter_title"], getGenreList()),
+                ),
+            )
+        } else {
+            filters.add(
+                Filter.Header(intl["genre_missing_warning"]),
+            )
+        }
+        if (hasProjectPage) {
+            filters.addAll(
+                mutableListOf<Filter<*>>(
+                    Filter.Separator(),
+                    Filter.Header(intl["project_filter_warning"]),
+                    Filter.Header(intl.format("project_filter_name", name)),
+                    ProjectFilter(intl["project_filter_title"], projectFilterOptions),
+                ),
+            )
+        }
+        return FilterList(filters)
+    }
+
+    override fun pageListParse(document: Document): List<Page> {
+        val scriptContent = document.selectFirst("script:containsData(ts_reader)")?.data()
+            ?: return super.pageListParse(document)
+        val jsonString = scriptContent.substringAfter("ts_reader.run(").substringBefore(");")
+        val tsReader = json.decodeFromString<TSReader>(jsonString)
+        val imageUrls = tsReader.sources.firstOrNull()?.images ?: return emptyList()
+
+        // Menggunakan URL resize
+        val resizeServiceUrl = getResizeServiceUrl()
+        return imageUrls.mapIndexed { index, imageUrl -> 
+            Page(index, document.location(), "$resizeServiceUrl$imageUrl")
+        }
+    }
+
+    override fun setupPreferenceScreen(screen: PreferenceScreen) {
+        val customUserAgentPref = EditTextPreference(screen.context).apply {
+            key = "custom_ua"
+            title = "Custom User-Agent"
+            summary = "Masukkan custom User-Agent Anda di sini."
+            setDefaultValue("Default User-Agent")
+        }
+        screen.addPreference(customUserAgentPref)
+
+        val resizeServicePref = EditTextPreference(screen.context).apply {
+            key = "resize_service_url"
+            title = "Resize Service URL"
+            summary = "Masukkan URL layanan resize gambar."
+            setDefaultValue("https://resize.sardo.work/?width=300&quality=75&imageUrl=")
+            dialogTitle = "Resize Service URL"
+            dialogMessage = "Masukkan URL layanan resize gambar. (default: https://resize.sardo.work/?width=300&quality=75&imageUrl=)"
+        }
+        screen.addPreference(resizeServicePref)
+
+        // Preference untuk mengubah base URL
+        val baseUrlPref = EditTextPreference(screen.context).apply {
+            key = BASE_URL_PREF
+            title = BASE_URL_PREF_TITLE
+            summary = BASE_URL_PREF_SUMMARY
+            setDefaultValue(baseUrl)
+            dialogTitle = BASE_URL_PREF_TITLE
+            dialogMessage = "Original: $baseUrl"
+
+            setOnPreferenceChangeListener { _, newValue ->
+                val newUrl = newValue as String
+                baseUrl = newUrl
+                preferences.edit().putString(BASE_URL_PREF, newUrl).apply()
+                summary = "Current domain: $newUrl" // Update summary untuk domain yang baru
+                true
+            }
+        }
+        screen.addPreference(baseUrlPref)
+    }
+
+    companion object {
+        private const val BASE_URL_PREF_TITLE = "Ubah Domain"
+        private const val BASE_URL_PREF = "overrideBaseUrl"
+        private const val BASE_URL_PREF_SUMMARY = "Update domain untuk ekstensi ini"
+    }
+
+    @Serializable
+    data class TSReader(
+        val sources: List<ReaderImageSource>,
+    )
+
+    @Serializable
+    data class ReaderImageSource(
+        val source: String,
+        val images: List<String>,
+    )
 }
