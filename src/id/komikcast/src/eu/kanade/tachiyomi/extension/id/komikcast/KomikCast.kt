f508e6b6c635c843b36de5df93816c733aff9792
imgview <komiklah007@gmail.com>
2024-11-09 20:43:50 +0700

    Update KomikCast.kt

--- src/id/komikcast/src/eu/kanade/tachiyomi/extension/id/komikcast/KomikCast.kt
+++ src/id/komikcast/src/eu/kanade/tachiyomi/extension/id/komikcast/KomikCast.kt
@@ -8,11 +8,13 @@ import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.Page
 import kotlinx.serialization.Serializable
 import android.app.Application
+import kotlinx.serialization.decodeFromString
 import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import okhttp3.Headers
+import eu.kanade.tachiyomi.source.ConfigurableSource
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.Request
