07e9f2ad6a24e8214d195ec0d761a49136d30ebb
imgview <komiklah007@gmail.com>
2024-11-15 02:38:37 +0700

    Update MGKomik.kt

--- src/id/mgkomik/src/eu/kanade/tachiyomi/extension/id/mgkomik/MGKomik.kt
+++ src/id/mgkomik/src/eu/kanade/tachiyomi/extension/id/mgkomik/MGKomik.kt
@@ -19,33 +19,26 @@ class MGKomik : Madara(
     override val mangaSubString = "komik"
 
     override fun headersBuilder() = super.headersBuilder().apply {
-    add("User-Agent", "Mozilla/5.0 (Linux; Android 10; Pixel 3 XL Build/QD1A.190805.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36")
-    add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8")
-    add("Accept-Encoding", "gzip, deflate, br")
-    add("Accept-Language", "en-US,en;q=0.9")
-    add("Referer", "https://mgkomik.id/")
-    add("Connection", "keep-alive")
-    add("Upgrade-Insecure-Requests", "1")
-    add("Sec-Fetch-Dest", "document")
-    add("Sec-Fetch-Mode", "navigate")
-    add("Sec-Fetch-Site", "same-origin")
-    add("Sec-Fetch-User", "?1")
-    add("Cache-Control", "max-age=0")
-}
-
-override val client = network.cloudflareClient.newBuilder()
-    .addInterceptor { chain ->
-        val request = chain.request()
-        
-        // Modifikasi untuk menghapus "X-Requested-With"
-        val headers = request.headers.newBuilder().apply {
-            removeAll("X-Requested-With")
-        }.build()
-
-        chain.proceed(request.newBuilder().headers(headers).build())
+        add("Sec-Fetch-Dest", "document")
+        add("Sec-Fetch-Mode", "navigate")
+        add("Sec-Fetch-Site", "same-origin")
+        add("Upgrade-Insecure-Requests", "1")
+        add("X-Requested-With", randomString((1..20).random())) // added for webview, and removed in interceptor for normal use
+        add("Referer", "$baseUrl/") // Add the Referer header
+        add("Accept-Language", "id-ID,id;q=0.9,en-US;q=0.9,en;q=0.7") // Add the Accept-Language header with en-US;q=0.9
     }
-    .rateLimit(5, 10)
-    .build()
+
+    override val client = network.cloudflareClient.newBuilder()
+        .addInterceptor { chain ->
+            val request = chain.request()
+            val headers = request.headers.newBuilder().apply {
+                removeAll("X-Requested-With")
+            }.build()
+
+            chain.proceed(request.newBuilder().headers(headers).build())
+        }
+        .rateLimit(9, 2)
+        .build()
 
     override fun popularMangaNextPageSelector() = ".wp-pagenavi span.current + a"
 
