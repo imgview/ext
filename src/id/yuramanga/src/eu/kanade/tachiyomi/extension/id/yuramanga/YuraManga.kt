f4fac1ce0ff8e03686c3a1b4800bd5e7ac005bd0
imgview <komiklah007@gmail.com>
2024-11-14 10:57:32 +0700

    Delete src directory

--- src/id/yuramanga/src/eu/kanade/tachiyomi/extension/id/yuramanga/YuraManga.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.id.yuramanga
-
-import eu.kanade.tachiyomi.multisrc.zmanga.ZManga
-import java.text.SimpleDateFormat
-
-class YuraManga : ZManga("YuraManga", "https://yuramanga.my.id", "id", SimpleDateFormat("dd/MM/yyyy")) {
-    // Moved from Madara to ZManga
-    override val versionId = 3
-}
