38415c2cda49437372193debcd3df828e5aeef3c
imgview <komiklah007@gmail.com>
2024-11-22 14:47:30 +0700

    Update Kace.kt

--- src/id/kace/src/eu/kanade/tachiyomi/extension/id/kace/Kace.kt
+++ src/id/kace/src/eu/kanade/tachiyomi/extension/id/kace/Kace.kt
@@ -1,85 +1,81 @@
 package eu.kanade.tachiyomi.extension.id.kace
 
-import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import eu.kanade.tachiyomi.network.GET
 import okhttp3.Request
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 
 class Kace : ParsedHttpSource() {
 
-    override val name = "Nama Project"
-    override val baseUrl = "https://contoh-manga.com"
+    override val name = "Kace"
+    override val baseUrl = "https://bacakomik.co"
     override val lang = "id"
     override val supportsLatest = true
 
-    // Selector untuk pencarian manga
-    override fun searchMangaSelector(): String = "div.search-item"
+    // Pencarian manga
+    override fun searchMangaSelector() = "div.bs"
 
-    // Parsing hasil pencarian
     override fun searchMangaFromElement(element: Element): SManga {
         val manga = SManga.create()
-        manga.title = element.select("h3.title").text()
+        manga.title = element.select("a").attr("title")
         manga.setUrlWithoutDomain(element.select("a").attr("href"))
-        manga.thumbnail_url = element.select("img.cover").attr("abs:src")
+        manga.thumbnail_url = element.select("img").attr("data-src")
         return manga
     }
 
-    // URL untuk pencarian
-    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
-        return GET("$baseUrl/search?q=$query&page=$page", headers)
+    override fun searchMangaRequest(page: Int, query: String): Request {
+        return GET("$baseUrl/page/$page/?s=$query", headers)
     }
 
-    // Selector untuk daftar terbaru
-    override fun latestUpdatesSelector(): String = "div.latest-update-item"
+    // Manga terbaru
+    override fun latestUpdatesSelector() = "div.bs"
 
-    // Parsing daftar terbaru
     override fun latestUpdatesFromElement(element: Element): SManga {
         val manga = SManga.create()
-        manga.title = element.select("h3.title").text()
+        manga.title = element.select("a").attr("title")
         manga.setUrlWithoutDomain(element.select("a").attr("href"))
-        manga.thumbnail_url = element.select("img.cover").attr("abs:src")
+        manga.thumbnail_url = element.select("img").attr("data-src")
         return manga
     }
 
-    // URL untuk daftar terbaru
     override fun latestUpdatesRequest(page: Int): Request {
-        return GET("$baseUrl/latest?page=$page", headers)
+        return GET("$baseUrl/manga/latest/page/$page", headers)
     }
 
-    // Selector untuk daftar chapter
-    override fun chapterListSelector(): String = "ul.chapters li.chapter"
+    // Detail manga
+    override fun mangaDetailsParse(document: Document): SManga {
+        val manga = SManga.create()
+        manga.title = document.select("h1").text()
+        manga.author = document.select("div.author-content").text()
+        manga.artist = document.select("div.artist-content").text()
+        manga.genre = document.select("div.genres-content a").joinToString { it.text() }
+        manga.description = document.select("div.description-summary").text()
+        manga.thumbnail_url = document.select("div.thumb img").attr("data-src")
+        return manga
+    }
 
-    // Parsing daftar chapter
+    // Daftar chapter
+    override fun chapterListSelector() = "li.wp-manga-chapter"
+
     override fun chapterFromElement(element: Element): SChapter {
         val chapter = SChapter.create()
         chapter.name = element.select("a").text()
         chapter.setUrlWithoutDomain(element.select("a").attr("href"))
-        chapter.date_upload = parseDate(element.select("span.date").text())
+        chapter.date_upload = System.currentTimeMillis() // Ubah sesuai kebutuhan
         return chapter
     }
 
-    // Parsing halaman gambar
+    // Halaman gambar
     override fun pageListParse(document: Document): List<Page> {
-        return document.select("div.page img")
-            .mapIndexed { i, element -> Page(i, "", element.attr("abs:src")) }
+        return document.select("div.page-break img")
+            .mapIndexed { i, element -> Page(i, "", element.attr("data-src")) }
     }
 
-    // Parsing detail manga
-    override fun mangaDetailsParse(document: Document): SManga {
-        val manga = SManga.create()
-        manga.title = document.select("h1.title").text()
-        manga.author = document.select("span.author").text()
-        manga.genre = document.select("div.genres").joinToString { it.text() }
-        manga.description = document.select("div.description").text()
-        manga.thumbnail_url = document.select("img.cover").attr("abs:src")
-        return manga
-    }
-    
-    private fun parseDate(date: String): Long {
-        // Implementasi parsing tanggal
-        return System.currentTimeMillis() // Contoh default
+    override fun imageUrlParse(document: Document): String {
+        throw UnsupportedOperationException("Not used.")
     }
 }
