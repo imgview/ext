0749a6aa68f954d9a5959c0995db371c4a4de4d0
imgview <komiklah007@gmail.com>
2024-12-02 16:37:56 +0700

    Update SirenKomik.kt

--- src/id/mangkomik/src/eu/kanade/tachiyomi/extension/id/mangkomik/SirenKomik.kt
+++ src/id/mangkomik/src/eu/kanade/tachiyomi/extension/id/mangkomik/SirenKomik.kt
@@ -33,6 +33,24 @@ class SirenKomik :
         )
         return FilterList(filters)
     }
+    
+        override val id = 8457447675410081142
+
+    override val hasProjectPage = true
+
+    override val seriesTitleSelector = "h1.judul-komik"
+    override val seriesThumbnailSelector = ".gambar-kecil img"
+    override val seriesGenreSelector = ".genre-komik a"
+    override val seriesAuthorSelector = ".keterangan-komik:contains(author) span"
+    override val seriesArtistSelector = ".keterangan-komik:contains(artist) span"
+
+    override fun chapterListSelector() = ".list-chapter a"
+
+    override fun chapterFromElement(element: Element) = SChapter.create().apply {
+        name = element.selectFirst(".nomer-chapter")!!.text()
+        date_upload = element.selectFirst(".tgl-chapter")?.text().parseChapterDate()
+        setUrlWithoutDomain(element.absUrl("href"))
+    }
 
     override fun pageListParse(document: Document): List<Page> {
         val scriptContent = document.selectFirst("script:containsData(SExtras.imgview)")?.data()
