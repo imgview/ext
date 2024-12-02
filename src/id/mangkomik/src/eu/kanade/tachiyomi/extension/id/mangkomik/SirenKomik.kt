d03a95a47853f19178dbc289be39383621d75759
imgview <komiklah007@gmail.com>
2024-12-02 15:33:02 +0700

    Update SirenKomik.kt

--- src/id/mangkomik/src/eu/kanade/tachiyomi/extension/id/mangkomik/SirenKomik.kt
+++ src/id/mangkomik/src/eu/kanade/tachiyomi/extension/id/mangkomik/SirenKomik.kt
@@ -2,59 +2,64 @@ package eu.kanade.tachiyomi.extension.id.mangkomik
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.source.model.Filter
-import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.Page
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.decodeFromString
-import okhttp3.Request
+import eu.kanade.tachiyomi.source.model.SChapter
 import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import kotlinx.serialization.Serializable
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-class SirenKomik :
-    MangaThemesia(
-        "Siren Komik",
-        "https://sirenkomik.my.id",
-        "id",
-        "/manga",
-    ) {
+class SirenKomik : MangaThemesia(
+    "Siren Komik",
+    "https://sirenkomik.my.id",
+    "id",
+    dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("id")),
+) {
+    override val id = 8457447675410081142
 
-    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
-        return if (query.isEmpty()) {
-            super.searchMangaRequest(page, query, filters)
-        } else {
-            GET("$baseUrl/?s=$query&page=$page", headers)
-        }
-    }
+    override val hasProjectPage = true
 
-    override fun getFilterList(): FilterList {
-        val filters = mutableListOf<Filter<*>>(
-            Filter.Header("Note: Can't be used with text search!"),
-            Filter.Separator(),
-        )
-        return FilterList(filters)
+    override val seriesTitleSelector = "h1.judul-komik"
+    override val seriesThumbnailSelector = ".gambar-kecil img"
+    override val seriesGenreSelector = ".genre-komik a"
+    override val seriesAuthorSelector = ".keterangan-komik:contains(author) span"
+    override val seriesArtistSelector = ".keterangan-komik:contains(artist) span"
+
+    override fun chapterListSelector() = ".list-chapter a"
+
+    override fun chapterFromElement(element: Element) = SChapter.create().apply {
+        name = element.selectFirst(".nomer-chapter")!!.text()
+        date_upload = element.selectFirst(".tgl-chapter")?.text().parseChapterDate()
+        setUrlWithoutDomain(element.absUrl("href"))
     }
 
     override fun pageListParse(document: Document): List<Page> {
+        // Cari elemen <script> yang mengandung 'SExtras.imgview'
         val scriptContent = document.selectFirst("script:containsData(SExtras.imgview)")?.data()
             ?: return super.pageListParse(document)
 
+        // Ekstrak JSON dari fungsi 'SExtras.imgview({...});'
         val jsonString = scriptContent.substringAfter("SExtras.imgview(").substringBefore(");")
+
+        // Parsing JSON ke dalam model data
         val sExtrasData = json.decodeFromString<SExtras>(jsonString)
-        val imageUrls: List<String> = sExtrasData.sources.firstOrNull()?.images ?: return emptyList()
 
-        return imageUrls.mapIndexed { index, imageUrl -> 
-            Page(index, document.location(), imageUrl)
-        }
+        // Ambil URL gambar dari sumber pertama
+        val imageUrls = sExtrasData.sources.firstOrNull()?.images ?: return emptyList()
+
+        // Konversi URL gambar menjadi daftar Page
+        return imageUrls.mapIndexed { index, imageUrl -> Page(index, document.location(), imageUrl) }
     }
-
-    @Serializable
-    data class SExtras(
-        val sources: List<ImageSource>,
-    )
-
-    @Serializable
-    data class ImageSource(
-        val source: String,
-        val images: List<String>,
-    )
 }
+
+@Serializable
+data class SExtras(
+    val sources: List<ImageSource>,
+)
+
+@Serializable
+data class ImageSource(
+    val source: String,
+    val images: List<String>,
+)
