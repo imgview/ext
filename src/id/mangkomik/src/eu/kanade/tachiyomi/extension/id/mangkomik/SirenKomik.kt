add8e3641abc83b344864d194b4d2a2657173569
imgview <komiklah007@gmail.com>
2024-12-12 23:48:04 +0700

    Update SirenKomik.kt

--- src/id/mangkomik/src/eu/kanade/tachiyomi/extension/id/mangkomik/SirenKomik.kt
+++ src/id/mangkomik/src/eu/kanade/tachiyomi/extension/id/mangkomik/SirenKomik.kt
@@ -1,26 +1,22 @@
 package eu.kanade.tachiyomi.extension.id.mangkomik
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import org.jsoup.nodes.Element
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.decodeFromString
-import okhttp3.Request
 import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 class SirenKomik :
     MangaThemesia(
-    "Siren Komik",
-    "https://sirenkomik.my.id",
-    "id",
-    dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("id")),
+        name = "Siren Komik",
+        baseUrl = "https://sirenkomik.my.id",
+        lang = "id",
+        dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("id"))
     ) {
-    
-        override val id = 8457447675410081142
+
+    override val id = 8457447675410081142
 
     override val hasProjectPage = true
 
@@ -38,11 +34,12 @@ class SirenKomik :
     }
 
     override fun pageListParse(document: Document): List<Page> {
-    val imageElements = document.select("img.ts-main-image") // Selector yang sesuai
-    if (imageElements.isEmpty()) return super.pageListParse(document)
+        val imageElements = document.select("img.ts-main-image")
+        if (imageElements.isEmpty()) return super.pageListParse(document)
 
-    return imageElements.mapIndexed { index, element ->
-        val imageUrl = element.attr("src") // Ambil URL gambar
-        Page(index, document.location(), imageUrl)
+        return imageElements.mapIndexed { index, element ->
+            val imageUrl = element.attr("src")
+            Page(index, document.location(), imageUrl)
+        }
     }
 }
